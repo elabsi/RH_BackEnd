f9f5a9504706d2a3bd943ca3ef15b7adf6e4acd8
red-ait <redouanmkdem@gmail.com>
2019-05-09 22:38:47 +0000

    aa

---
+++ src/main/java/ma/ensa/entities/Pointage.java
@@ -0,0 +1,97 @@
+package ma.ensa.entities;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Entity
+public class Pointage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Temporal(TemporalType.DATE)
+	private Date dateReel;
+	@Temporal(TemporalType.TIME)
+	private Date heureReel;
+	private int modeReel;
+	@Temporal(TemporalType.DATE)
+	private Date dateModifie;
+	@Temporal(TemporalType.TIME)
+	private Date heureModifier;
+	private int modeModifie;
+	private int status;
+	@ManyToOne
+	private Collaborateur collaborateur;
+	
+	public Collaborateur getCollaborateur() {
+		return collaborateur;
+	}
+	public void setCollaborateur(Collaborateur collaborateur) {
+		this.collaborateur = collaborateur;
+	}
+	public Pointage() {
+		super();
+	}
+	public Pointage(Long id, Date dateReel, Date heureReel, int modeReel, Date dateModifie, Date heureModifier,
+			int modeModifie, int status) {
+		super();
+		this.id = id;
+		this.dateReel = dateReel;
+		this.heureReel = heureReel;
+		this.modeReel = modeReel;
+		this.dateModifie = dateModifie;
+		this.heureModifier = heureModifier;
+		this.modeModifie = modeModifie;
+		this.status = status;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getDateReel() {
+		return dateReel;
+	}
+	public void setDateReel(Date dateReel) {
+		this.dateReel = dateReel;
+	}
+	public Date getHeureReel() {
+		return heureReel;
+	}
+	public void setHeureReel(Date heureReel) {
+		this.heureReel = heureReel;
+	}
+	public int getModeReel() {
+		return modeReel;
+	}
+	public void setModeReel(int modeReel) {
+		this.modeReel = modeReel;
+	}
+	public Date getDateModifie() {
+		return dateModifie;
+	}
+	public void setDateModifie(Date dateModifie) {
+		this.dateModifie = dateModifie;
+	}
+	public Date getHeureModifier() {
+		return heureModifier;
+	}
+	public void setHeureModifier(Date heureModifier) {
+		this.heureModifier = heureModifier;
+	}
+	public int getModeModifie() {
+		return modeModifie;
+	}
+	public void setModeModifie(int modeModifie) {
+		this.modeModifie = modeModifie;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+
+}
